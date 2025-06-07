d2885402e030fbfb653a9e4356ad07cd9d0ba4d3
zLuizGuilhermez <luizerae.s@gmail.com>
2025-06-07 20:12:25 -0300

    atualização do banco vetorial adicionando IMC, ajuste logica da ia

--- app/ia/ia_logica.py
+++ app/ia/ia_logica.py
@@ -3,15 +3,12 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core.settings import Settings
 
-# Configuração dos modelos
 Settings.llm = Ollama(model="gemma2:2b")
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
-# Carregamento e indexação dos documentos
-folder_path = "app/data/dados_diabetes"
-documentos = SimpleDirectoryReader(folder_path).load_data()
+documentos = SimpleDirectoryReader("app/data/dados_diabetes").load_data()
 index = VectorStoreIndex.from_documents(documentos)
-query_engine = index.as_query_engine(similarity_top_k=2)
+query_engine = index.as_query_engine()
 
 def avaliar_dados_paciente_ia(idade: int, glicose: float, imc: float, pressao: int, insulina=None, espessura_pele=None, pedigree=None, gestacoes=None) -> str:
     prompt = (
@@ -19,28 +16,14 @@ def avaliar_dados_paciente_ia(idade: int, glicose: float, imc: float, pressao: i
         f"Idade: {idade}\n"
         f"Glicose: {glicose}\n"
         f"IMC: {imc}\n"
-        f"Pressão arterial: {pressao}\n"
+        f"Pressão arterial: {pressao}\n\n"
+        f"Com base no conteúdo dos documentos, avalie se esse paciente pode estar com diabetes. "
+        f"Responda apenas com uma das opções: "
+        f"1. 'Sim, é possível que o paciente tenha diabetes.' "
+        f"2. 'Não, é improvável que o paciente tenha diabetes.'"
+
     )
-    if insulina is not None:
-        prompt += f"Insulina: {insulina}\n"
-    if espessura_pele is not None:
-        prompt += f"Espessura da pele: {espessura_pele}\n"
-    if pedigree is not None:
-        prompt += f"Função hereditária: {pedigree}\n"
-    if gestacoes is not None:
-        prompt += f"Número de gestações: {gestacoes}\n"
-    prompt += ("\nCom base no conteúdo dos documentos, avalie se esse paciente pode estar com diabetes. "
-               "Responda apenas com uma das opções: "
-               "Sim, é possível que o paciente tenha diabetes. "
-               "Não, é improvável que o paciente tenha diabetes. "
-               "Responda 'Sim' se houver indícios razoáveis, caso contrário responda 'Não'.")
 
     resposta = query_engine.query(prompt)
-    resposta_str = resposta.response.strip().lower()
-    if resposta_str.startswith('sim'):
-        return 'Sim, é possível que o paciente tenha diabetes.'
-    elif resposta_str.startswith('não') or resposta_str.startswith('nao'):
-        return 'Não, é improvável que o paciente tenha diabetes.'
-    else:
-        return resposta.response.strip()
+    return resposta.response.strip()
 
--- main.py
+++ main.py
@@ -13,4 +13,3 @@ app.add_middleware(
 )
 
 app.include_router(router)
-
